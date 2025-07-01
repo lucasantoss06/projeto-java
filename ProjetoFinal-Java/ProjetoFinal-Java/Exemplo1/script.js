b3c3c043dc84137771ef56ae912b6e8821020280
lucasantoss06 <lucas.gabrielpnr@gmail.com>
2025-06-30 21:14:48 -0300

    Add files via upload

---
+++ ProjetoFinal-Java/ProjetoFinal-Java/Exemplo1/script.js
@@ -0,0 +1,24 @@
+﻿document.getElementById("formulario").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // Coletar valores
+  const valores = [];
+  for (let i = 1; i <= 5; i++) {
+    const valor = document.getElementById(`valor${i}`).value.trim();
+    if (valor === "") {
+      alert(`O campo Valor ${i} está vazio.`);
+      return;
+    }
+    valores.push(valor);
+  }
+
+  // Criar conteúdo do TXT
+  const conteudo = valores.map((v, i) => `Valor ${i + 1}: ${v}`).join("\n");
+
+  // Criar e baixar o arquivo TXT
+  const blob = new Blob([conteudo], { type: "text/plain;charset=utf-8" });
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(blob);
+  link.download = "valores.txt";
+  link.click();
+});
---
+++ ProjetoFinal-Java/ProjetoFinal-Java/script.js
@@ -0,0 +1,11 @@
+function abrirExemplo1(){
+    window.location.href = "Exemplo1/index.html";
+}
+
+function abrirExemplo2(){
+    window.location.href = "Exemplo2/index.html";
+}
+
+function abrirExemplo3(){
+    window.location.href = "Exemplo3/index.html";
+}
